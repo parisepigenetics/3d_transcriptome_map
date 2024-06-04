d779890661498f13dbf03be49866cca0db8719e5
Costas Bouyioukos <cbouyio@gmail.com>
2024-06-04 14:26:53 +0200

    Implement the modular figure drawing of the significant genes trace. Untested.

--- great_3d/plotly_graphs.py
+++ great_3d/plotly_graphs.py
@@ -11,12 +11,12 @@ from great_3d import timing
 COLOURS = plotly.colors.qualitative.Pastel + plotly.colors.qualitative.Safe + plotly.colors.qualitative.Vivid
 
 
-def get_significant_corr_genes(corSumsDict, coef=2):
+def get_significant_corr_genes(corSumsDict, coef):
     """Calculate statistical tests and return a list of significantly
     correlated genes from a sums_of_correlations dictionary.
     - Args:
     - `corSumsDict`: The dictionary of sums_of_correlations.
-    - `coef`: Optional. The coefficient for determining significance (default: 2).
+    - `coef`: The coefficient for determining significance (default: 2).
 
     - Return:
     - A list of significantly correlated genes.
@@ -40,9 +40,9 @@ def visualise_genome_3D(genome_coords):
     - Args:
     - `genome_coords_file`: The file path to the genome coordinates file.
     - Return:
-    - The trace of the genoome 3D contour.
+    - The trace of the genome 3D contour.
     """
-    # Build the genome contur trace
+    # Build the genome contour trace
     pos_dt = genome_coords
     chroms = pos_dt.loc[:, "chr"].tolist()
     # Generate the chromosome traces
@@ -63,8 +63,10 @@ def visualise_genome_3D(genome_coords):
             hoverinfo="text",
             hovertext=htxt,
             mode="lines+markers",
-            opacity=0.55,
+            opacity=0.385,
             showlegend=True)
+    global colourG
+    colourG=COLOURS[-(i+1)]  # FIXME that works ONLY for single chromosome file!
     return traceChr
 
 
@@ -85,14 +87,14 @@ def visualise_genes(pos_df):
     return go.Scatter3d(x=X, y=Y, z=Z,
                         ids=pos_df.index.values,
                         mode="markers",
-                        opacity=0.68,
-                        marker=dict(size=5, color="lightseagreen", line=dict(width=2, color='darkmagenta')),
+                        opacity=0.62,
+                        marker=dict(size=3, color=colourG, line=dict(width=1, color=colourG)),
                         hoverinfo='text',
                         hovertext=htxt,
                         showlegend=False)
 
 
-def visulise_correlation(position_df, correlation_dict, corl):
+def visualise_correlation(position_df, correlation_dict, corl):
     """Generate the correlation visualisation trace.
 
     Arguments:
@@ -111,8 +113,8 @@ def visulise_correlation(position_df, correlation_dict, corl):
     corrA = pos_df.loc[:, "corrA"].tolist()
     chroms = pos_df.loc[:, "chr"].tolist()
     # Construct the hover text list
-    htext = [f"{n}<br>{m}<br>{c:3f}<br>{s}" for n, m, c, s in zip(pos_df.index, chroms, corr, nearGenes)]
-    htextA = [f"{n}<br>{m}<br>{c:3f}<br>{s}" for n, m, c, s in zip(pos_df.index, chroms, corrA, nearGenes)]
+    htext = [f"{n}, {m}<br>{c:3f}<br>{s}" for n, m, c, s in zip(pos_df.index, chroms, corr, nearGenes)]
+    htextA = [f"{n}, {m}<br>{c:3f}<br>{s}" for n, m, c, s in zip(pos_df.index, chroms, corrA, nearGenes)]
     # Extract the coordinates
     X = pos_df.loc[:, "X"]
     Y = pos_df.loc[:, "Y"]
@@ -126,7 +128,7 @@ def visulise_correlation(position_df, correlation_dict, corl):
                             hoverinfo="text",
                             hovertext=htext,
                             mode="markers",
-                            opacity=1,
+                            opacity=0.62,
                             marker=dict(size=4, color=corr, colorscale="RdYlBu_r", showscale=True, colorbar=dict(orientation='h', thickness=4, xpad=30)))
     if corl == "corrA":
         # Absolute correlation trace
@@ -136,44 +138,45 @@ def visulise_correlation(position_df, correlation_dict, corl):
                             hoverinfo="text",
                             hovertext=htextA,
                             mode="markers",
-                            opacity=1,
+                            opacity=0.62,
                             marker=dict(size=4, color=corrA, colorscale="Hot_r", showscale=True, colorbar=dict(orientation='h', thickness=4, xpad=30)))
 
 
 @timing
-def visulise_significant_genes():
-    # Significant correlation trace
-    signifGenes = get_significant_corr_genes(correlation_dict)
-    # print("Significant Genes:\n")
-    # pp = pprint.PrettyPrinter(indent=2)
-    # pp.pprint(signifGenes)  #FIXME remove these two comment printing lines
+def visualise_significant_genes(pos_df, correlation_dict, coeff=2):
+    """Produce the significantly correlated genes trace.
+
+    Arguments:
+        position_df -- A dataframe of the 3D gene positions.
+        correlation_dict -- A Gene->Gene->[corr, absCorr] dictionary with the correlation values.
+    Returns:
+        A scatter3D track with the significantly correlated genes.
+    """
+    # Compute significant genes
+    signifGenes = get_significant_corr_genes(correlation_dict, coeff)
+    # Get the selected genes from the position DF
     posDF_sign = pos_df.loc[signifGenes, :]
     nearGenes = []
     for gene_ref in signifGenes:
         posDF_sign.loc[gene_ref, "Corr"] = correlation_dict[gene_ref][0]
-        # Append the list of selectes genes with a string
+        # Append the list of selected genes with a string
         nearGenes.append('<br>'.join(correlation_dict[gene_ref][2]))
     corr = posDF_sign.loc[:, "corr"].tolist()
     chroms = posDF_sign.loc[:, "chr"].tolist()
     # Construct the hover text list
-    htext = [f"{n}<br>{m}<br>{c:3f}<br>{s}" for n, m, c, s in zip(posDF_sign.index, chroms, corr, nearGenes)]
-    traceSign = go.Scatter3d(
-        x=posDF_sign.loc[:, "X"],
-        y=posDF_sign.loc[:, "Y"],
-        z=posDF_sign.loc[:, "Z"],
-        ids=posDF_sign.index.values,
-        name="Sign_Corr",
-        hoverinfo="text",
-        hovertext=htext,
-        mode="markers",
-        opacity=0.5,
-        marker=dict(size=7, color=corr, colorscale="RdYlBu_r", showscale=True),
-        showlegend=True)
-    return traceSign
+    htext = [f"{n}, {m}<br>{c:3f}<br>{s}" for n, m, c, s in zip(posDF_sign.index, chroms, corr, nearGenes)]
+    return go.Scatter3d(x=posDF_sign.loc[:,"X"], y=posDF_sign.loc[:,"Y"], z=posDF_sign.loc[:,"Z"],
+                        ids=posDF_sign.index.values,
+                        name="Sign_Corr",
+                        hoverinfo="text",
+                        hovertext=htext,
+                        mode="markers",
+                        opacity=0.385,
+                        marker=dict(size=6, color=corr, colorscale="RdYlBu_r", line=dict(width=1, color='darkmagenta'), showscale=True, colorbar=dict(orientation='h', thickness=4, xpad=30)))
 
 
 @timing
-def visualise_user_genes(user_genes):
+def visualise_user_genes(user_genes, pos_df, correlation_dict):
     # User specified trace
     if user_genes is not None:
         with user_genes as fh:
